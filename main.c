0387bf8c5bc0d17e3527ad9140bd876eef465844
Daniel Savage <dansvg@gmail.com>
2013-03-19 17:24:38 -0700

    added 2,5 curve

--- main.c
+++ main.c
@@ -902,6 +902,36 @@ void update(double delta)
 				pushTriangle(&t);
 				t.type = TRIANGLE;
 			}
+			else if (buddy(x, y, 2) && buddy(x, y, 5) && !buddy(x, y, 6) && !buddy(x, y, 7) && !buddy(x, y, 0) && !buddy(x, y, 1))
+			{
+				t.color = adjacencyMatrix[x][y].color;
+				t.a.x = x*16 + 8;
+				t.a.y = y*16;
+				t.b.x = x*16 + 16;
+				t.b.y = y*16 + 8;
+				t.c.x = x*16 + 8;
+				t.c.y = y*16 + 16;
+				pushTriangle(&t);
+				t.a.x = x*16 + 8;
+				t.a.y = y*16;
+				t.b.x = x*16;
+				t.b.y = y*16 + 16;
+				t.c.x = x*16 + 8;
+				t.c.y = y*16 + 16;
+				pushTriangle(&t);
+				t.a.x = x*16 + 8;
+				t.a.y = y*16;
+				t.b.x = x*16;
+				t.b.y = y*16 + 8;
+				t.c.x = x*16;
+				t.c.y = y*16 + 16;
+				t.type = CONVEX;
+				pushTriangle(&t);
+				t.type = CONCAVE;
+				t.color = adjacencyMatrix[x-1][y].color;
+				pushTriangle(&t);
+				t.type = TRIANGLE;
+			}
 			else
 			{
 
@@ -924,14 +954,17 @@ void update(double delta)
 			//bottom left corner
 			if (x == 0 || !buddy(x-1, y, 4))
 			{
-				t.color = adjacencyMatrix[x][y].color;
-				t.a.x = x*16;
-				t.a.y = y*16 + 8;
-				t.b.x = x*16 + 8;
-				t.b.y = y*16 + 16;
-				t.c.x = x*16;
-				t.c.y = y*16 + 16;
-				pushTriangle(&t);
+				if (!(buddy(x, y, 2) && buddy(x, y, 5) && !buddy(x, y, 6) && !buddy(x, y, 7) && !buddy(x, y, 0) && !buddy(x, y, 1)))
+				{
+					t.color = adjacencyMatrix[x][y].color;
+					t.a.x = x*16;
+					t.a.y = y*16 + 8;
+					t.b.x = x*16 + 8;
+					t.b.y = y*16 + 16;
+					t.c.x = x*16;
+					t.c.y = y*16 + 16;
+					pushTriangle(&t);
+				}
 			}
 			if (buddy(x, y, 6) && !buddy(x-1, y, 4))
 			{
